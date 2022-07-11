8c1025e2aa4c86ceae4de512ed1cd3d61e8553ed
Kirill <sonik3000.83@gmail.com>
2022-07-11 13:55:11 +0300

    mvnjava

---
+++ src/main/java/BonusService.java
@@ -0,0 +1,12 @@
+public class BonusService {
+    public long calculate(long amount, boolean registred) {
+        int percent = registred ? 3:1;
+        long bonus = amount * percent / 100;
+        long limit = 500;
+        if (bonus > limit) {
+            bonus= limit;
+        }
+        return bonus;
+
+    }
+}
